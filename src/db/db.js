272e8cb9f0029bd79457b2e201f8990e104e2c10
V-Icaro <venicius.icarosr@gmail.com>
2021-04-26 23:45:23 -0300

    Iniciando Backend

---
+++ src/db/db.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql')
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'me',
+    password: 'secret',
+    database: 'my_db'
+});
+
+function connectionDb() {
+    connection.connect((err) => {
+        if (err) {
+            console.error('error connecting: ' + err.stack);
+            return false;
+        }
+
+        return connection
+    });
+}
+
+module.exports = { connectionDb }
